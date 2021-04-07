e889066058df8bcaa47f153fcf3bc6162a19367a
Bill <bill.llach1801@gmail.com>
2021-04-06 22:20:59 -0500

    Update configuration of cors and session_store initializers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-
+    #For local
     allow do
       origins "http://127.0.0.1:3000"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
-  
+    #For the web
     allow do
-      origins "https://your-production-app-here.com"
-      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+      origins "*"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: false
     end
 
 end
